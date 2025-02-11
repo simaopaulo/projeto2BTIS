39f19695c5e4f138a038db1916327431ea337be7
Simão Paiva de Paulo BTIS 2023/2026 <5490@eclisboa.net>
2025-02-11 17:25:30 +0000

    https://github.com/simaopaulo/projeto2BTIS

---
+++ src/MathHelper/Main.java
@@ -0,0 +1,23 @@
+package MathHelper;
+
+public class Main {
+
+    public static void menu() {
+        System.out.println("============================");
+        System.out.println("=           Menu:          =");
+        System.out.println("============================");
+        System.out.println("= 1- Próximo Número        =");
+        System.out.println("= 2- Juntar nome e apelido =");
+        System.out.println("= 3- Maior Número          =");
+        System.out.println("= 4- Soma dos Números      =");
+        System.out.println("= 5- Média dos Números     =");
+        System.out.println("= 0- Sair                  =");
+        System.out.println("============================");
+        System.out.println("============================");
+    }
+
+    public static void main(String[] args) {
+
+        menu();
+    }
+}
---
+++ src/MathHelper/MathHelper.java
@@ -0,0 +1,24 @@
+package MathHelper;
+
+public class MathHelper {
+
+    public static int numeroSeguinte(int numero) {
+        return 1;
+    }
+
+    public static void juntarNome(String nome, String apelido) {
+        System.out.println(nome + " " + apelido);
+    }
+
+    public static int maior(int[] numeros) {
+        return 1;
+    }
+
+    public static int soma(int[] numeros){
+        return 1;
+    }
+
+    public static int media(double[] numeros){
+        return 1;
+    }
+}
