9884b0816491c35ff9ef7243b013507701bfd38f
Simão Paiva de Paulo BTIS 2023/2026 <5490@eclisboa.net>
2025-02-11 18:39:52 +0000

    Corrigido2 MathHelper.java

--- src/MathHelper/Main.java
+++ src/MathHelper/Main.java
@@ -1,5 +1,9 @@
 package MathHelper;
 
+import java.util.Scanner;
+
+////////////////////////////////////////////////////////////////////////////////////////
+
 public class Main {
 
     public static void menu() {
@@ -13,11 +17,69 @@ public class Main {
         System.out.println("= 5- Média dos Números     =");
         System.out.println("= 0- Sair                  =");
         System.out.println("============================");
-        System.out.println("============================");
+        System.out.println("Escolhe uma opção: ");
     }
 
-    public static void main(String[] args) {
 
-        menu();
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public static void main(String[] args) {
+        int escolha;
+        int numero;
+        String nome;
+        String apelido;
+        String juntarnomes;
+        //sair
+        int sair;
+        int sairmesmo = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+        menu(); // Menu começa
+        escolha = scanner.nextInt();
+        scanner.nextLine(); // comer /n
+
+        switch (escolha) {
+            case 1:
+                System.out.println("Introduz um número: ");
+                numero = scanner.nextInt();
+                numero = MathHelper.numeroSeguinte(numero);
+                System.out.println("A resposta é: " + numero);
+                break;
+
+            case 2:
+                System.out.println("Introduz um Nome: ");
+                nome = scanner.nextLine();
+
+                System.out.println("Introduz um Apelido: ");
+                apelido = scanner.nextLine();
+
+                juntarnomes = MathHelper.juntarNome(nome, apelido);
+
+                System.out.println("O nome completo é: " + juntarnomes);
+                break;
+
+            case 3:
+
+                break;
+
+            case 4:
+
+                break;
+
+            case 5:
+
+                break;
+
+            case 0:
+
+                break;
+
+            default:
+                System.out.println("Opção invalida, tente novamente.");
+                break;
+                }
+        } while (sairmesmo != 1);
     }
 }
